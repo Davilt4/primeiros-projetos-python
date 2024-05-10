755e1296d02d3b0af5a87a13fd38275cbe124d28
Davi Leite <106539266+Davilt4@users.noreply.github.com>
2024-05-10 09:01:44 -0300

    Update senha_aleatoria.py

--- senha_aleatoria.py
+++ senha_aleatoria.py
@@ -1,4 +1,5 @@
 import random
+
 letras_minusculas = "abcdefghijklmnopqrstuvwxyz"
 letras_maisculas = letras_minusculas.upper()
 numeros = "0123456789"
@@ -11,10 +12,14 @@ while True:
             tamanho_maisculo = (tamanho//3)
             tamanho_minusculo = (tamanho//3)
             tamanho_numeros = (tamanho//3)
+
+        
         else:
             tamanho_maisculo = (tamanho//3)+(tamanho%3)
             tamanho_minusculo = (tamanho//3)
             tamanho_numeros = (tamanho//3)
+
+        
         def gerar_senha():
             caractere_maiusculo ="".join(random.choices(letras_maisculas,k=(tamanho_maisculo)))
             dois_numeros = "".join(random.choices(numeros, k=(tamanho_minusculo)))
@@ -22,13 +27,19 @@ while True:
             senha = caractere_maiusculo + dois_numeros + cinco_letras_minusculas
             senha_embaralhada = "".join(random.sample(senha, len(senha)))
             print(len(senha_embaralhada))
-            return senha_embaralhada   
+            return senha_embaralhada
+            
         print(f"A senha gerada é: {gerar_senha()}") 
+        
     else:
         print("A senha deve ter pelo menos 3 caracteres")
     continuar = input("Deseja continuar(s/n)? ").lower()
+
+    
     if continuar in "sS" or continuar[0] == "s":
         continue
+
+    
     elif continuar in 'nN' or continuar[0] == "n":
         print("Programa encerrado.")
         break
